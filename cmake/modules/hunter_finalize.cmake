1b1ef65e7caa2a2e456bf2cfe2c4658b1f9f0ab6
Kyle Ambroff <kambroff@linkedin.com>
2018-03-27 18:19:15 -0700

    Revert "Comment-out broken check. Update gate submodule."
    This reverts commit 84c81f03cce9bfcf89adbfefffb2de494ec1cf3b.

--- cmake/modules/hunter_finalize.cmake
+++ cmake/modules/hunter_finalize.cmake
@@ -46,13 +46,11 @@ macro(hunter_finalize)
     set(_cxx_enabled TRUE)
   endif()
 
-  # FIXME: This doesn't quite work for some reason. Need to investigate,
-  # but I'm just disabling this check for now since it's wrong.
-  #if(_c_enabled AND NOT CMAKE_C_ABI_COMPILED)
-  #  hunter_fatal_error(
-  #      "ABI not detected for C compiler" WIKI "error.abi.detection.failure"
-  #  )
-  #endif()
+  if(_c_enabled AND NOT CMAKE_C_ABI_COMPILED)
+    hunter_fatal_error(
+        "ABI not detected for C compiler" WIKI "error.abi.detection.failure"
+    )
+  endif()
 
   if(_cxx_enabled AND NOT CMAKE_CXX_ABI_COMPILED)
     hunter_fatal_error(
