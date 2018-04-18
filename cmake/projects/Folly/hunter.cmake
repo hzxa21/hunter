3ebf77f78c9c56ac14b6b215deccc1a655ec8bba
Kyle Ambroff <kambroff@linkedin.com>
2018-04-18 15:15:26 -0700

    Fix Folly build on Linux.

--- cmake/projects/Folly/hunter.cmake
+++ cmake/projects/Folly/hunter.cmake
@@ -11,6 +11,13 @@ hunter_add_version(
   SHA1 7f4eb81bb284788a861b0f834a022b74a25f42f1
 )
 
+hunter_add_version(
+  PACKAGE_NAME Folly
+  VERSION "2018.04.18.00"
+  URL "https://github.com/ambroff/folly/archive/7c51aae270a5eb1fbad161d88f4e4dee715c9757.zip"
+  SHA1 d3a263034b46a36523d8ded209be58e502c9edce
+)
+
 if (APPLE)
   hunter_cmake_args(Folly CMAKE_ARGS FOLLY_USE_LIBCPP=TRUE)
 endif()
